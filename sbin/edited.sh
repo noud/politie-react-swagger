7d3d19d29636747be18eccaabcf7ff04cf3c51af
Noud de Brouwer <noud4@home.nl>
2020-12-30 00:06:03 +0100

    edited.sh

--- sbin/edited.sh
+++ sbin/edited.sh
@@ -5,3 +5,6 @@ export APP_NAME=my-app
 cp src-edited/config/entrypoint.js ${APP_NAME}/src/config/
 cp ${APP_NAME}/src/index.js index.pre-gen.js
 cp src-edited/index.js ${APP_NAME}/src/
+cp src-edited/serviceWorker.js ${APP_NAME}/src/
+cp src-edited/welcome.css ${APP_NAME}/src/
+cp src-edited/Welcome.js ${APP_NAME}/src/
